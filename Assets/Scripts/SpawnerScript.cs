13f90fa76ac5878e1372255c3ccbf833235d11c2
arispap78 <73643527+arispap78@users.noreply.github.com>
2021-03-24 20:20:32 +0200

    my first UNITY project

---
+++ Assets/Scripts/SpawnerScript.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+//the check box for the shape of the collision area 
+public enum CollisionType
+{
+    Sphere,
+    Box
+}
+
+//the Spawner
+public class SpawnerScript : MonoBehaviour 
+{
+    //the variable for the choice of the check box
+    public CollisionType type;
+    //the time for the creation of a sphere in the beginning
+    [Range(0.1f, 5)]
+	public float spawnTime = 3f;
+    //the number of spheres that will be created
+    public float max_num_particles = 20;
+    //the multiplier of the force of the wind
+    [Range(-500, 500)]
+    public float constantForce = 20;
+    //the multiplier for the forces of attraction and repulsion
+    [Range(0, 10)]
+    public float pullForceMultiplier = 100;
+    //the boolean for the gravity
+    public bool sphereGravity = true;
+    //the multiplier for the gravity
+    public float gravityMultiplier = 100;
+    //the object that with make the attraction or the repulsion
+    public GameObject pull0;
+    public GameObject pull1;
+    //the object for each sphere
+	private GameObject generatedSphere;
+    //the distance of each sphere from the objects with the attraction and repulsion forces
+    float distance1;
+    float distance2;
+    //the list of the spheres
+    public List<SphereMovement> myShperes = new List<SphereMovement>();
+
+	void Start () 
+    {
+        //find the objects with the attraction and repulsion forces
+        pull0 = GameObject.Find("Pull0");
+        pull1 = GameObject.Find("Pull1");
+        //the routine to create a sphere
+        StartCoroutine(WaitAndSpawn(spawnTime));
+    }
+    //the creation of the spheres
+    private IEnumerator WaitAndSpawn(float waitTime)
+    {
+        while (myShperes.Count < max_num_particles)
+        {
+            //wait for a while for the next sphere
+            yield return new WaitForSeconds(waitTime);
+            //the shape of each sphere is taken from the prefab
+            generatedSphere = Instantiate(Resources.Load("Prefabs/Sphere") as GameObject);
+            //the position of the sphere
+            generatedSphere.transform.position = transform.position;
+            //add to the list
+            myShperes.Add(generatedSphere.GetComponent<SphereMovement>());
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        foreach (SphereMovement sphere in myShperes) 
+        {
+            //the horizontal force of the wind 
+            sphere.myRb.AddForce(Vector3.right * constantForce);
+            //the gravity(with a choice and a multiplier)
+            if (sphereGravity) 
+                sphere.myRb.AddForce(Vector3.down * gravityMultiplier);
+            //the choice of the check box
+            if (type == CollisionType.Box && !sphere.isBox) 
+            {
+                sphere.isBox = true;
+            }
+
+            else if (type == CollisionType.Sphere && sphere.isBox) 
+            {
+                sphere.isBox = false;
+            }
+            //if the collision area is a sphere
+            if (!sphere.isBox)
+            {
+                Vector3 dir1 = sphere.transform.position - pull0.transform.position;
+                Vector3 dir2 = sphere.transform.position - pull1.transform.position;
+                distance1 = dir1.magnitude;
+                distance2 = dir2.magnitude;
+
+                sphere.transform.position =
+                    Vector3.MoveTowards(sphere.transform.position, pull0.transform.position, -pullForceMultiplier * Time.deltaTime / distance1);
+                sphere.transform.position =
+                    Vector3.MoveTowards(sphere.transform.position, pull1.transform.position, -pullForceMultiplier * Time.deltaTime / distance2);
+            }
+            //if the collision area is a cube
+            if (sphere.isBox)
+                sphere.transform.position =
+                    Vector3.MoveTowards(sphere.transform.position, pull0.transform.position, pullForceMultiplier * Time.deltaTime);
+                sphere.transform.position =
+                    Vector3.MoveTowards(sphere.transform.position, pull1.transform.position, pullForceMultiplier * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/SphereMovement.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+
+//class for the movement of the spheres
+public class SphereMovement : MonoBehaviour
+{
+    //multiplies the forces
+    public float forceMultiplier = 10;
+    //the limit of every axis
+    public float axisLimit = 4;
+    //the radius of the sphere
+    public float radius = 5;
+    //the point in space where the velocity of each sphere with go to
+    Vector3 pointA;
+    //the rigidbody of the sphere
+    public Rigidbody myRb;
+    //the boolean if the limits are of a cube or of a sphere
+    public bool isBox = true;
+
+
+    void Start()
+    {
+        //get the rigidbody of the sphere
+        myRb = this.gameObject.GetComponent<Rigidbody>();
+        //the random variables of the pointA
+        float randomX = Random.Range(-12, 12);
+        float randomY = Random.Range(-12, 12);
+        float randomZ = Random.Range(-12, 12);
+        //the point for the direction of the velocity
+        pointA = new Vector3(randomX, randomY, randomZ);
+        //the velocity of the sphere
+        myRb.velocity = pointA;
+    }
+
+    void FixedUpdate()
+    {
+        //if it is a cube
+        if (isBox) 
+        {
+            BoxCollision();
+        }
+        //if it is a sphere
+        else 
+        {
+            SphereCollision();
+        }
+
+    }
+
+    //the collision of the spheres inside the cube
+    public void BoxCollision() 
+    {
+        //if the sphere is off the limits in the x axis
+        if (Mathf.Abs(transform.position.x) >= axisLimit)
+        {
+            //reverse the value of x 
+            pointA = new Vector3(pointA.x * -1, pointA.y, pointA.z);
+            //put the sphere inside the cube in case it is outside
+            transform.position = new Vector3(transform.position.x * 3.99f / Mathf.Abs(transform.position.x)
+                , transform.position.y, transform.position.z);
+        }
+        //if the sphere is off the limits in the y axis
+        if (Mathf.Abs(transform.position.y) >= axisLimit)
+        {
+            //reverse the value of y 
+            pointA = new Vector3(pointA.x, pointA.y * -1, pointA.z);
+            //put the sphere inside the cube in case it is outside
+            transform.position = new Vector3(transform.position.x, transform.position.y * 3.99f / Mathf.Abs(transform.position.y)
+                , transform.position.z);
+        }
+        //if the sphere is off the limits in the z axis
+        if (Mathf.Abs(transform.position.z) >= axisLimit)
+        {
+            //reverse the value of z
+            pointA = new Vector3(pointA.x, pointA.y, pointA.z * -1);
+            //put the sphere inside the cube in case it is outside
+            transform.position = new Vector3(transform.position.x, transform.position.y,
+                transform.position.z * 3.99f / Mathf.Abs(transform.position.z));
+        }
+        //multiply the velocity to the pointA with 0.05 coefficient of depreciation
+        myRb.velocity = pointA * forceMultiplier * 0.95f;
+    }
+
+    //the collision of the spheres inside the sphere
+    public void SphereCollision() 
+    {
+        //if a sphere pass the limits of the sphere(x*x+y*y+z*z=radius*radius)
+        if (System.Math.Pow(transform.position.x, 2) + System.Math.Pow(transform.position.y, 2) + System.Math.Pow(transform.position.z, 2) >= System.Math.Pow(radius, 2))
+        {
+            //reverse the values of the coordinates 
+            pointA = new Vector3(pointA.x * -1, pointA.y * -1, pointA.z * -1);
+            //put the sphere inside the sphere in case it is outside
+            transform.position = new Vector3(transform.position.x * 0.9f, transform.position.y * 0.9f, transform.position.z * 0.9f);
+        }
+        //multiply the velocity to the pointA with 0.05 coefficient of depreciation
+        myRb.velocity = pointA * forceMultiplier * 0.95f;
+    } 
+}
+
